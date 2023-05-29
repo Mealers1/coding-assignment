3bc9cde9ed7d70484af428e927e0b0a9286e333c
Mealers1 <bull19_leo12@yahoo.com>
2023-05-29 10:18:00 -0500

    first commit

---
+++ src/week04/JavaWeek4CodingAssignment.java
@@ -0,0 +1,12 @@
+package week04;
+
+public class JavaWeek4CodingAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+StringBuilder fullName = new StringBuilder("Tom");
+fullName.append (" Petty");
+System.out.println(fullName.toString());
+	}
+
+}
